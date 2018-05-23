121ab3c9273ef002129cbcda8570be29f70ea9e4
Bernie Reiter <ockham@raz.or.at>
2018-05-23 17:23:21 +0200

    Formatting

--- transforms/dispatch-request-ex.js
+++ transforms/dispatch-request-ex.js
@@ -1,24 +1,20 @@
 /** @format */
-export default function transformer(file, api) {
-    const j = api.jscodeshift;
-  
-    return j(file.source)
-      .find(j.CallExpression, { callee: { type: 'Identifier', name: 'dispatchRequest' } } )
-      .forEach(path => {
-        j(path).replaceWith( () => {
-          const [ fetch, onSuccess, onError ] = path.node.arguments;
-          return j.callExpression( j.identifier( "dispatchRequestEx" ),
-          [
-            j.objectExpression( [
-              j.objectProperty( j.identifier( "fetch" ), fetch ),
-              j.objectProperty( j.identifier( "onSuccess" ), onSuccess ),
-              j.objectProperty( j.identifier( "onError" ), onError )
-            ] )
-          ]
-          )
-          }
-        );
-      })
-      .toSource();
-  }
-  
+export default function transformer( file, api ) {
+	const j = api.jscodeshift;
+
+	return j( file.source )
+		.find( j.CallExpression, { callee: { type: 'Identifier', name: 'dispatchRequest' } } )
+		.forEach( path => {
+			j( path ).replaceWith( () => {
+				const [ fetch, onSuccess, onError ] = path.node.arguments;
+				return j.callExpression( j.identifier( 'dispatchRequestEx' ), [
+					j.objectExpression( [
+						j.objectProperty( j.identifier( 'fetch' ), fetch ),
+						j.objectProperty( j.identifier( 'onSuccess' ), onSuccess ),
+						j.objectProperty( j.identifier( 'onError' ), onError ),
+					] ),
+				] );
+			} );
+		} )
+		.toSource();
+}

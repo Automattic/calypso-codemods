89399fca0553d1cc2c18bb57f93dc52929b98ac6
Bernie Reiter <ockham@raz.or.at>
2018-05-24 17:55:50 +0200

    Update snapshot tests

--- tests/dispatch-request-ex/with-options.js
+++ tests/dispatch-request-ex/with-options.js
@@ -1,4 +1,51 @@
-dispatchRequest( dismissBackup, backupSilentlyDismissed, backupDismissFailed, {
-        fromApi: fromBackupDismiss,
-        onProgress: updateProgress,
+export const fromApi = response => {
+	if ( ! response.data ) {
+		throw new Error( 'missing settings' );
+	}
+
+	return normalizeSettings( response.data );
+};
+
+const toApi = settings => filterSettingsByActiveModules( sanitizeSettings( settings ) );
+
+const receiveJetpackOnboardingSettings = ( { dispatch }, { siteId }, settings ) => {
+	dispatch( updateJetpackSettings( siteId, settings ) );
+};
+export const requestJetpackSettings = ( { dispatch }, action ) => {
+	const { siteId, query } = action;
+
+	return dispatch(
+		http(
+			{
+				apiVersion: '1.1',
+				method: 'GET',
+				path: '/jetpack-blogs/' + siteId + '/rest-api/',
+				query: {
+					path: '/jetpack/v4/settings/',
+					query: JSON.stringify( query ),
+					json: true,
+				},
+			},
+			action
+		)
+	);
+};
+
+export const announceRequestFailure = ( { dispatch, getState }, { siteId } ) => {
+	const state = getState();
+	const url = getSiteUrl( state, siteId ) || getUnconnectedSiteUrl( state, siteId );
+	const noticeOptions = {
+		id: `jpo-communication-error-${ siteId }`,
+	};
+
+	if ( url ) {
+		noticeOptions.button = translate( 'Visit site admin' );
+		noticeOptions.href = trailingslashit( url ) + 'wp-admin/admin.php?page=jetpack';
+	}
+
+	return dispatch( errorNotice( translate( 'Something went wrong.' ), noticeOptions ) );
+};
+
+dispatchRequest( requestJetpackSettings, receiveJetpackOnboardingSettings, announceRequestFailure, {
+	fromApi,
 } );

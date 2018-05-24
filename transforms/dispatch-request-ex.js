03d5969ff5543aa7c8c145aa1d30d604c5b39b05
Bernie Reiter <ockham@raz.or.at>
2018-05-24 17:59:47 +0200

    Drop dispatch from all handlers

--- transforms/dispatch-request-ex.js
+++ transforms/dispatch-request-ex.js
@@ -9,18 +9,20 @@ export default function transformer( file, api ) {
 			j( path ).replaceWith( () => {
 				const [ fetch, onSuccess, onError ] = path.node.arguments;
 
-				root.find( j.VariableDeclarator, { id: { name: fetch.name } } ).forEach( p => {
-					return j( p ).replaceWith( a => {
-						const fn = a.node.init;
-						const { params, body } = fn;
+				[ fetch, onSuccess, onError ].forEach( handler => {
+					root.find( j.VariableDeclarator, { id: { name: handler.name } } ).forEach( p => {
+						return j( p ).replaceWith( a => {
+							const fn = a.node.init;
+							const { params, body } = fn;
 
-						params.shift();
-						j( body )
-							.find( j.CallExpression, { callee: { name: 'dispatch' } } )
-							.replaceWith( ret => {
-								return ret.node.arguments;
-							} );
-						return a.node;
+							params.shift();
+							j( body )
+								.find( j.CallExpression, { callee: { name: 'dispatch' } } )
+								.replaceWith( ret => {
+									return ret.node.arguments;
+								} );
+							return a.node;
+						} );
 					} );
 				} );
 

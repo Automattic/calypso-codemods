5e9b03c99313f23ec36a6fcefa6a353139e6f815
Bernie Reiter <ockham@raz.or.at>
2018-05-24 17:21:04 +0200

    Drop first argument from fetch function

--- transforms/dispatch-request-ex.js
+++ transforms/dispatch-request-ex.js
@@ -1,12 +1,23 @@
 /** @format */
 export default function transformer( file, api ) {
 	const j = api.jscodeshift;
+	const root = j( file.source );
 
-	return j( file.source )
+	return root
 		.find( j.CallExpression, { callee: { type: 'Identifier', name: 'dispatchRequest' } } )
 		.forEach( path => {
 			j( path ).replaceWith( () => {
 				const [ fetch, onSuccess, onError ] = path.node.arguments;
+
+				root.find( j.VariableDeclarator, { id: { name: fetch.name } } ).forEach( p => {
+					return j( p ).replaceWith( a => {
+						const fn = a.node.init;
+						const { params } = fn;
+						params.shift();
+						return a.node;
+					} );
+				} );
+
 				return j.callExpression( j.identifier( 'dispatchRequestEx' ), [
 					j.objectExpression( [
 						j.objectProperty( j.identifier( 'fetch' ), fetch ),
